232127586d1036580f071daefbada8de21fb0cc3
Eirik Mo <eirik@mowebdev.com>
2023-08-11 02:21:03 +0200

    add erronous examples

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import { WritableAtom, atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
+export function atomWithToggle(
+  initialValue?: boolean
+): WritableAtom<boolean, boolean | undefined> {
+  const anAtom = atom(initialValue, (get, set, nextValue?: boolean) => {
+    const update = nextValue ?? !get(anAtom)
+    set(anAtom, update)
+  })
+
+  return anAtom as WritableAtom<boolean, boolean | undefined>
+}
+
 export function atomWithToggleAndStorage(
   key: string,
   initialValue?: boolean,
